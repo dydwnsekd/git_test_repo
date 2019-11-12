eb6227cad975ceb3f5fd20f7e68370aaa0f7636e
dydwnsekd <dydwnsekd123@gmail.com>
2019-11-12 12:03:09 +0900

    git pull / remove parameter 입력 받도록 수정

--- gitpull.sh
+++ gitpull.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
 
-path=$1
-git_url=$2
+parameter_check()
+{
+    echo "Usage: $0 [git master path] [git url]"
+}
 
 # 배열에 내가 찾는 값이 있는지 확인하는 함수
-in_array() {
+in_array()
+{
     local needle array value
     needle="${1}"; shift; array=("${@}")
     for value in ${array[@]}; do [ "${value}" == "${needle}" ] && echo "true" && return; done
     echo "false"
 }
 
+path='./'
+git_url=''
+
+if [ $# -ne 2 ]
+then
+    parameter_check
+    exit 0
+else if [ $2 -eq '' ]
+then
+    echo "input git_url"
+    parameter_check
+    exit 0
+fi
+
+path=$1
+git_url=$2
+
+cd $path
+
 # local과 원격 코드, branch 동기화
 git fetch && git fetch -p && git reset --hard head && git pull
 
 # git_url 수정 필요
-git_url="git@github.com:dydwnsekd/git_test_repo.git"
+# git_url="git@github.com:dydwnsekd/git_test_repo.git"
 
 # git branch 목록
 branchs=$(git branch -r 2>/dev/null | awk '{print $1}' | tr '/' ' ' | awk '{print $2}')
@@ -31,18 +53,18 @@ branch_dirs=$(ls)
 # branch들 모두 확인
 for branch in $branchs
 do
-        # master을 제외한 다른 branch들에 대해서만 확인할 수 있도록 체크
-        branch_check=`in_array $branch ${except_branch[@]}`
-        
-        # branch directory가 이미 있을 경우 true(branch directory 가 존재) / false(branch는 있는데 dirtory가 없음)
-        dir_check=`in_array $branch ${branch_dirs[@]}` 
+    # master을 제외한 다른 branch들에 대해서만 확인할 수 있도록 체크
+    branch_check=`in_array $branch ${except_branch[@]}`
+    
+    # branch directory가 이미 있을 경우 true(branch directory 가 존재) / false(branch는 있는데 dirtory가 없음)
+    dir_check=`in_array $branch ${branch_dirs[@]}` 
 
-        if [ "${branch_check}" == "false" ]; then
-                
-                if [ "${dir_check}" == "true" ]; then
-                        cd $branch && git fetch && git reset --hard head && git pull && cd ..
-                else
-                        git clone -b $branch $git_url $branch
-                fi
+    if [ "${branch_check}" == "false" ]; then
+            
+        if [ "${dir_check}" == "true" ]; then
+                cd $branch && git fetch && git reset --hard head && git pull && cd ..
+        else
+                git clone -b $branch $git_url $branch
         fi
+    fi
 done
--- gitremove.sh
+++ gitremove.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+parameter_check()
+{
+    echo "Usage: $0 [git master path]"
+}
+
 # 배열에 내가 찾는 값이 있는지 확인하는 함수
-in_array() {
+in_array()
+{
     local needle array value
     needle="${1}"; shift; array=("${@}")
     for value in ${array[@]}; do [ "${value}" == "${needle}" ] && echo "true" && return; done
     echo "false"
 }
+
+path='./'
+
+if [ $# -ne 1 ]
+then
+    parameter_check
+    exit 0
+fi
+
+path=$1
+
+cd $path
 
 # local과 원격 코드, branch 동기화
 git fetch && git fetch -p
@@ -25,16 +43,17 @@ branch_dirs=$(ls)
 # branch들 모두 확인
 for dir in $branch_dirs
 do
-        # master을 제외한 다른 branch들에 대해서만 확인할 수 있도록 체크
-        branch_check=`in_array $dir ${except_branch[@]}`
-        
-        # directory명이 branch중에 있으면 true 없으면 false
-        dir_check=`in_array $dir ${branchs[@]}` 
+    # master을 제외한 다른 branch들에 대해서만 확인할 수 있도록 체크
+    branch_check=`in_array $dir ${except_branch[@]}`
+    
+    # directory명이 branch중에 있으면 true 없으면 false
+    dir_check=`in_array $dir ${branchs[@]}` 
 
-        if [ "${branch_check}" == "false" ]; then
-                
-                if [ "${dir_check}" == "false" ]; then
-                        rm -rf $dir
-                fi
+    if [ "${branch_check}" == "false" ]; then
+        
+        if [ "${dir_check}" == "false" ]; then
+                rm -rf $dir
         fi
+    fi
 done
+
