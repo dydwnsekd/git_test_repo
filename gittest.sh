93e0babc28f78b62cde2a5d0ebb75f286362f8d5
dydwnsekd <dydwnsekd123@gmail.com>
2019-11-21 18:16:48 +0900

    gittest.sh 수정

--- gittest.sh
+++ gittest.sh
@@ -7,7 +7,18 @@ in_array() {
     echo "false"
 }
 
+old_branchs=$(cat branch_list 2>/dev/null)
+echo $old_branchs
+
+# local과 원격 코드, branch 동기화
+git fetch && git fetch -p && git reset --hard head && git pull
+
 # git branch 목록
 branchs=$(git branch -r 2>/dev/null | awk '{print $1}' | tr '/' ' ' | awk '{print $2}')
 
-echo $branchs "\n " >> branch_list 
+echo $branchs > branch_list 
+
+for old_branch in $old_branchs
+do
+    echo $old_branch
+done
