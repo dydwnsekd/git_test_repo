ec4c76cfd138ab22cc47569e50d94b72f8d3ea3e
dydwnsekd <dydwnsekd123@gmail.com>
2019-12-27 17:51:30 +0900

    불필요 파일 제거

--- directory_sync.sh
+++ directory_sync.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-origin_path="/Users/dydwnsekd/Documents/git/git_test/master/"
-compare_path="/Users/dydwnsekd/Documents/git/git_test/copy/"
+parameter_check()
+{
+    echo "Usage: $0 [origin dir path] [compare dir path]"
+}
 
-file_list=$(ls -R)
+if [ $# -ne 2 ]
+then
+    parameter_check
+    exit 0
+fi
+
+origin_path=$1
+compare_path=$2
 
 origin_dir_path=$origin_path
 compare_dir_path=$compare_path
+
+cd $origin_path
+file_list=$(ls -R)
 
 for file in $file_list
 do
@@ -25,14 +37,45 @@ do
 
             if [ "${result_diff}" == "Files" ];
             then
-                echo $origin_full_path
+                #echo $origin_full_path
                 rm -rf $compare_full_path
                 cp $origin_full_path $compare_full_path
             fi
         else
-            echo $origin_full_path "is new file"
+            #echo $origin_full_path "is new file"
             cp $origin_full_path $compare_full_path
         fi
     fi
 done
+
+unset dir_path
+unset compare_dir_path
+unset compare_full_path
+
+cd $compare_path
+file_list=$(ls -R)
+
+origin_dir_path=$origin_path
+compare_dir_path=$compare_path
+
+for file in $file_list
+do
+    if [ "${file:(-1)}" == ":" ];
+    then
+        dir_path=${file:2:${#file}-3}/
+        origin_dir_path=$origin_path$dir_path
+        compare_dir_path=$compare_path$dir_path
+    else
+        origin_full_path=$origin_dir_path$file
+        compare_full_path=$compare_dir_path$file
+
+        if [ -e $origin_full_path ];
+        then
+            :
+        else
+            rm -rf $compare_full_path
+        fi
+    fi
+done
+
 
